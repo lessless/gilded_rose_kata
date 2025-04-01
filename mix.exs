7426439bd24df5a88c8968d4244feee490206e56
Yevhenii Kurtov <yevhenii.kurtov@dyad.net>
2025-04-01 12:29:40 +0100

    Install Coveralls

--- mix.exs
+++ mix.exs
@@ -2,8 +2,29 @@ defmodule GildedRose.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :gilded_rose,
-     version: "0.0.1",
-     elixir: "~> 1.0"]
+    [
+      app: :gilded_rose,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
+    ]
   end
+
+  defp deps do
+    [
+      {:excoveralls, "~> 0.18", only: :test}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
